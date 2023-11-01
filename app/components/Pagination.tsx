74ec9444749c5078b3baffa420a5245044e2eff5
Baz125 <103266154+Baz125@users.noreply.github.com>
2023-11-01 18:56:02 +0100

    Update dependencies

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { Button, Flex, Text } from "@radix-ui/themes";
+import { Button, Flex, Text, Box } from "@radix-ui/themes";
 import {
   ChevronLeftIcon,
   ChevronRightIcon,
