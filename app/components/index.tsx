5f0ba093cd8cc2519bfcfa143cd5492e7ee1273f
Baz125 <103266154+Baz125@users.noreply.github.com>
2023-11-01 18:09:10 +0100

    Bugfix: Fixed typo in component exports

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -1,7 +1,5 @@
-import Link from "./Link";
+export { default as Link } from "./Link";
 export { default as ErrorMessage } from "./ErrorMessage";
-export { default as IssueStatusBadge } from "./Spinner";
+export { default as IssueStatusBadge } from "./IssueStatusBadge";
 export { default as Spinner } from "./Spinner";
 export { default as Skeleton } from "./Skeleton";
-
-export { Link };
