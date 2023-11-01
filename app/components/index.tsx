3989adb0905f0276effb1e82c5aa0f852df950a7
Baz125 <103266154+Baz125@users.noreply.github.com>
2023-11-01 18:02:12 +0100

    Bugfix: Change how Link is exported from index.tsx

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -1,5 +1,7 @@
-export { default as Link } from "./Link";
+import Link from "./Link";
 export { default as ErrorMessage } from "./ErrorMessage";
 export { default as IssueStatusBadge } from "./Spinner";
 export { default as Spinner } from "./Spinner";
 export { default as Skeleton } from "./Skeleton";
+
+export { Link };
